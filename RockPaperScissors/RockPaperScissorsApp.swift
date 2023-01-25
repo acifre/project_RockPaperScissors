0416a5c4fd8239defe9460e58c3bc76cb8497d24
acifre <78503928+acifre@users.noreply.github.com>
2023-01-25 14:23:07 -0600

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Anthony Cifre on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
