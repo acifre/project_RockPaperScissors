c680075b39a05377d3d34491a3b74d97461d1dc3
acifre <78503928+acifre@users.noreply.github.com>
2023-01-25 15:18:22 -0600

    Start working on UI

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -45,7 +45,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            
+            Image(systemName: "globe")
             Text("Rock Paper Scissors!")
         }
         .padding()
